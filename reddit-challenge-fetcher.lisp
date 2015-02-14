20406cde8a7757c06c12332dae652faa690a27f0
flpa <flpa.dev@zoho.com>
2015-02-14 22:38:56 +0100

    drafts: struct 'challenge', split-title..

--- reddit-challenge-fetcher.lisp
+++ reddit-challenge-fetcher.lisp
@@ -2,5 +2,28 @@
 
 (in-package #:reddit-challenge-fetcher)
 
-;;; "reddit-challenge-fetcher" goes here. Hacks and glory await!
+(defvar *solutions-directory* "~/code/misc/reddit-daily/")
+
+(defparameter *sample-challenge* (first (get-subreddit-new "dailyprogrammer")))
+
+(defun split-title (title)
+  "Split the TITLE string of a challenge into the various components and
+   returns them as multple values.
+   The components are (in this order): 
+   - Date (YYYY-MM-DD)
+   - Number
+   - Category (e.g. Easy)
+   - Name"
+  title)
+
+;; TODO: override user agent?
+
+(defstruct challenge "Basic structure specifying a challenge."
+  name
+  number
+  difficulty ;;enum via init arg checking? actually, there also others: All, Practical Exercise ... Raise condition to be handled with restart?
+  description
+  )
+
+
 
